08c48e70030e13584965379854bc28233029f050
Kwin Sirikwin <k.sirikw@gmail.com>
2020-10-10 20:23:33 +0700

    Initial Commit

---
+++ Flash Chat SwiftUI/Flash_Chat_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_Chat_SwiftUIApp.swift
+//  Flash Chat SwiftUI
+//
+//  Created by Kwin Sirikwin on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Flash_Chat_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
